12c96263094bbd61da1de9b57fb8df2c15f4d8a3
Filip Dyba <f.dyba28@gmail.com>
2020-11-22 10:25:20 +0100

    Dodanie funkcji dedykowanych do odczytu/zapisu pozycji robota do pliku csv

--- Lab1/read_write_csv.py
+++ Lab1/read_write_csv.py
@@ -1,7 +1,7 @@
 import csv
 import datetime
 
-def csv_reader(filepath):
+def csv_EncoderReader(filepath):
 	with open(filepath) as csvfile:
 		reader = csv.DictReader(csvfile, delimiter=';', quotechar='|')
 		posL = []
@@ -17,10 +17,33 @@ def csv_reader(filepath):
 			velR.append(row['velR'])
 	return t,posL,posR,velL,velR
 	
-def csv_writer(filepath,time,posL,posR,velL,velR):
+def csv_EncoderWriter(filepath,time,posL,posR,velL,velR):
 	with open(filepath,'w') as csvfile:
 		fieldnames = ['#time', 'posL', 'posR', 'velL', 'velR']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';',quotechar='|')
 		writer.writeheader()
 		for i in range(len(posL)):
 			writer.writerow({'#time': time[i], 'posL': posL[i], 'posR': posR[i], 'velL': velL[i], 'velR': velR[i]})
+
+def csv_PoseReader(filepath):
+	with open(filepath) as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=';', quotechar='|')
+		x = []
+		y = []
+		z = []
+		t = []
+		for row in reader:
+			t.append(datetime.datetime.strptime(row['#time'],'%H:%M:%S.%f'))
+			x.append(row['x'])
+			y.append(row['y'])
+			z.append(row['z'])
+	return t,x,y,z
+	
+def csv_PoseWriter(filepath,time,x,y,z):
+	with open(filepath,'w') as csvfile:
+		fieldnames = ['#time', 'x', 'y', 'z']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';',quotechar='|')
+		writer.writeheader()
+		for i in range(len(posL)):
+			writer.writerow({'#time': time[i], 'x': x[i], 'y': y[i], 'z': z[i]})
+
