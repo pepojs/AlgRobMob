169ebfd7f3c9de946f6d3c04c29e172cde55f7a8
Agata <agata.smolag@op.pl>
2020-11-22 16:16:03 +0100

    dodanie trajektorii

--- Lab1/trajektoria.py
+++ Lab1/trajektoria.py
@@ -1,11 +1,21 @@
 from drive import RosAriaDriver
+import math
 robot=RosAriaDriver('/PIONIER6') # w miejsce X wstaw numer robota
 
+t=6.30
+s=1.15
+
 def go_right_square(robot):
-    robot.SetSpeedLR(0.05,0.05,10)
-    robot.SetSpeedLR(-0.05,0.05,10)
+    robot.SetSpeedLR(s/t,s/t,t)
+    robot.SetSpeedLR(0,math.pi/(2*t),t)
+    robot.SetSpeedLR(s/t,s/t,t)
+    robot.SetSpeedLR(0,math.pi/(2*t),t)
+    robot.SetSpeedLR(s/t,s/t,t)
+    robot.SetSpeedLR(0,math.pi/(2*t),t)
+    robot.SetSpeedLR(s/t,s/t,t)
     
 
+go_right_square(robot)
 [(posL,posR),(vL,vR)]=robot.ReadEncoder()
 posL, posR
 
