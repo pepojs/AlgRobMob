ef22f2631f897de9d3f31f1a2840b1fa2be31e74
filipdyba <f.dyba28@gmail.com>
2020-11-23 08:28:27 +0100

    Integracja z zapisywaniem do pliku csv

--- Lab1/trajektoria.py
+++ Lab1/trajektoria.py
@@ -1,6 +1,8 @@
 from drive import RosAriaDriver
 import datetime
 import math
+from read_write_csv import *
+
 robot=RosAriaDriver('/PIONIER6') # w miejsce X wstaw numer robota
 
 t=4.20
@@ -18,8 +20,12 @@ def go_right_square(robot):
     robot.SetSpeedLR(0,math.pi/(2*t_obrotu),t_obrotu)
 
 go_right_square(robot)
-[(posL,posR),(vL,vR)]=robot.ReadEncoder()
-[(x,y,theta)]=robot.ReadPose()
+while True:
+	measure_time=datetime.datetime.now()
+	[(posL,posR),(vL,vR)]=robot.ReadEncoder()
+	[(x,y,theta)]=robot.ReadPose()
+	csv_EncoderWriter('Enkoder.csv',measure_time,posL,posR,vL,vR)
+	csv_PoseWriter('Robot.csv',measure_time,x,y,theta)
 
 
 time_a=datetime.datetime.now()
