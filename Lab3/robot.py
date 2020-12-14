1c38ef62979bba82a2242f71dd85be1bf8a55ed1
Piotr Bogdoł <bogdolpiotr@gmail.com>
2020-12-14 14:08:34 +0100

    odczyty z robota

---
+++ Lab3/robot.py
@@ -0,0 +1,27 @@
+from time import sleep
+import datetime
+from drive import RosAriaDriver
+
+# Enter robot number instead of X 
+robot=RosAriaDriver('/PIONIER4')
+
+ar=[]
+robot.ResetPose()
+
+t0=datetime.datetime.now()
+# Zapisanie 3 zestawow danych 
+for x in range(0,12):
+        rec={}
+        scan=robot.ReadLaser()
+        pose=robot.GetPose()
+        rec['pose']=pose
+        rec['scan']=scan
+        t1=datetime.datetime.now()-t0
+        rec['time']=t1.seconds+t1.microseconds/1000000.0
+        ar.append(rec)
+        robot.SetSpeed(0, 0.5/2.0, 2.0)
+        nb = raw_input('Press enter')
+
+import json
+with open('data_stereo1.json','w') as json_data_file:
+    json.dump(ar,json_data_file)
